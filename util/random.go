659d8c768316e32a3a93214af3a956242d1ea91d
Yusuf Manshur <ymanshur@gmail.com>
2023-03-02 15:51:08 +0700

    add random owner money and currency util

--- util/random.go
+++ util/random.go
@@ -28,3 +28,20 @@ func RandomString(n int) string {
 	}
 	return sb.String()
 }
+
+// RandomOwner generates a random owner name
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+// RandomMoney generates a random amount of money
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+// RandomCurrency generates a random currency code
+func RandomCurrency() string {
+	currencies := []string{"EUR", "USD", "CAD"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
