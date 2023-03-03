c0c6cee5c58c3715a3be27f2908173b6d90a97f2
Yusuf Manshur <ymanshur@gmail.com>
2023-03-03 19:20:02 +0700

    restyle init schema up migration

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -1,24 +1,24 @@
 CREATE TABLE "accounts" (
-                            "id" bigserial PRIMARY KEY,
-                            "owner" varchar NOT NULL,
-                            "balance" bigint NOT NULL,
-                            "currency" varchar NOT NULL,
-                            "created_at" timestamptz NOT NULL DEFAULT (now())
+    "id" bigserial PRIMARY KEY,
+    "owner" varchar NOT NULL,
+    "balance" bigint NOT NULL,
+    "currency" varchar NOT NULL,
+    "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "entries" (
-                           "id" bigserial PRIMARY KEY,
-                           "account_id" bigint NOT NULL,
-                           "amount" bigint NOT NULL,
-                           "created_at" timestamptz DEFAULT (now())
+    "id" bigserial PRIMARY KEY,
+    "account_id" bigint NOT NULL,
+    "amount" bigint NOT NULL,
+    "created_at" timestamptz DEFAULT (now())
 );
 
 CREATE TABLE "transfers" (
-                             "id" bigserial PRIMARY KEY,
-                             "from_account_id" bigint NOT NULL,
-                             "to_account_id" bigint NOT NULL,
-                             "amount" bigint NOT NULL,
-                             "created_at" timestamptz NOT NULL DEFAULT (now())
+     "id" bigserial PRIMARY KEY,
+     "from_account_id" bigint NOT NULL,
+     "to_account_id" bigint NOT NULL,
+     "amount" bigint NOT NULL,
+     "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE INDEX ON "accounts" ("owner");
