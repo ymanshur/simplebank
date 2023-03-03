6472315a9ba814562b18436828599e10ea7e718d
Yusuf Manshur <ymanshur@gmail.com>
2023-03-03 17:23:50 +0700

    test store's TransferTx method

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -13,15 +13,20 @@ const (
 	dbSource = "postgresql://postgres:postgres@localhost:5432/simple_bank?sslmode=disable"
 )
 
-var testQueries *Queries
+var (
+	testQueries *Queries
+	testDB      *sql.DB
+)
 
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDriver, dbSource)
+	var err error
+
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
 
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	os.Exit(m.Run())
 }
