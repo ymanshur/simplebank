bee9b11999f0ea0596d9ee632611bd52461f1dfa
Yusuf Manshur <ymanshur@gmail.com>
2023-03-07 19:27:36 +0700

    complete store mock stubs for get account test

--- api/account_test.go
+++ api/account_test.go
@@ -1,30 +1,39 @@
 package api
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
 	mockdb "github.com/ymanshur/simplebank/db/mock"
 	db "github.com/ymanshur/simplebank/db/sqlc"
 	"github.com/ymanshur/simplebank/util"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestServer_GetAccount(t *testing.T) {
+	account := randomAccount()
+
+	// TODO: define table driven test sets
+
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
 	store := mockdb.NewMockStore(ctrl)
 
-	// TODO: build stubs
+	// build stubs
+	store.EXPECT().
+		GetAccount(gomock.Any(), gomock.Eq(account.ID)).
+		Times(1).
+		Return(account, nil)
 
 	server := NewServer(store)
 
 	recorder := httptest.NewRecorder()
-
-	account := randomAccount()
 
 	requestTarget := fmt.Sprintf("/accounts/%d", account.ID)
 	request := httptest.NewRequest(http.MethodGet, requestTarget, nil)
@@ -32,6 +41,7 @@ func TestServer_GetAccount(t *testing.T) {
 
 	// check response
 	require.Equal(t, http.StatusOK, recorder.Code)
+	requireBodyMatchAccount(t, recorder.Body, account)
 }
 
 func randomAccount() db.Account {
@@ -41,4 +51,14 @@ func randomAccount() db.Account {
 		Balance:  util.RandomMoney(),
 		Currency: util.RandomCurrency(),
 	}
+}
+
+func requireBodyMatchAccount(t *testing.T, body *bytes.Buffer, account db.Account) {
+	data, err := io.ReadAll(body)
+	require.NoError(t, err)
+
+	var gotAccount db.Account
+	err = json.Unmarshal(data, &gotAccount)
+	require.NoError(t, err)
+	require.Equal(t, account, gotAccount)
 }
