66141b192581784ef0559008f719e77f4517a0e4
Yusuf Manshur <ymanshur@gmail.com>
2023-03-10 14:07:25 +0700

    register custom currency binding tag

--- api/server.go
+++ api/server.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	db "github.com/ymanshur/simplebank/db/sqlc"
 )
 
@@ -14,6 +16,11 @@ type Server struct {
 // NewServer creates a new HTTP server and set up routing.
 func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
+
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
 	router := gin.Default()
 
 	router.POST("/accounts", server.createAccount)
