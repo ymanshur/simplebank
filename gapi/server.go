e5210579d29784315447e64aec4d362775e101f2
Yusuf Manshur <ymanshur@gmail.com>
2023-04-10 15:46:13 +0700

    add grpc server implementation for create and login user

---
+++ gapi/server.go
@@ -0,0 +1,52 @@
+package gapi
+
+import (
+	"fmt"
+	db "github.com/ymanshur/simplebank/db/sqlc"
+	"github.com/ymanshur/simplebank/pb"
+	"github.com/ymanshur/simplebank/pkg/token"
+	"github.com/ymanshur/simplebank/pkg/util"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+)
+
+// Server serves gRPC requests for our banking service.
+type Server struct {
+	pb.UnimplementedSimpleBankServer
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+	rpc        *grpc.Server
+}
+
+// NewServer creates a new gRPC server.
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
+
+	server.rpc = grpc.NewServer()
+	pb.RegisterSimpleBankServer(server.rpc, server)
+	reflection.Register(server.rpc)
+
+	return server, nil
+}
+
+func (server *Server) Start(address string) error {
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatal("cannot create listener:", err)
+	}
+
+	log.Printf("start gRPC server at %s", listener.Addr().String())
+	return server.rpc.Serve(listener)
+}
