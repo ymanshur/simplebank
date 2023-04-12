8d6460dbf9133d286e0c87e23796aa6df161e0f4
Yusuf Manshur <ymanshur@gmail.com>
2023-04-12 14:16:14 +0700

    refactor user gapi

---
+++ gapi/rpc_create_user.go
@@ -0,0 +1,68 @@
+package gapi
+
+import (
+	"context"
+	"github.com/lib/pq"
+	db "github.com/ymanshur/simplebank/db/sqlc"
+	"github.com/ymanshur/simplebank/pb"
+	"github.com/ymanshur/simplebank/pkg/util"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest) (*pb.CreateUserResponse, error) {
+	violations := validateCreateUserRequest(req)
+	if violations != nil {
+		return nil, invalidArgumentError(violations)
+	}
+
+	hashedPassword, err := util.HashPassword(req.GetPassword())
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to hash password: %s", err)
+	}
+
+	arg := db.CreateUserParams{
+		Username:       req.GetUsername(),
+		FullName:       req.GetFullName(),
+		Email:          req.GetEmail(),
+		HashedPassword: hashedPassword,
+	}
+
+	user, err := server.store.CreateUser(ctx, arg)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "unique_violation":
+				return nil, status.Errorf(codes.AlreadyExists, "username already exists: %s", err)
+
+			}
+		}
+		return nil, status.Errorf(codes.Internal, "failed to create user: %s", err)
+	}
+
+	rsp := &pb.CreateUserResponse{
+		User: convertUser(user),
+	}
+	return rsp, nil
+}
+
+func validateCreateUserRequest(req *pb.CreateUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := validateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, fieldViolation("username", err))
+	}
+
+	if err := validatePassword(req.GetPassword()); err != nil {
+		violations = append(violations, fieldViolation("password", err))
+	}
+
+	if err := validateFullName(req.GetFullName()); err != nil {
+		violations = append(violations, fieldViolation("full_name", err))
+	}
+
+	if err := validateEmail(req.GetEmail()); err != nil {
+		violations = append(violations, fieldViolation("email", err))
+	}
+
+	return violations
+}
