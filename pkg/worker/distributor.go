4cf22faefc550f2b3770ba681cf74a805d0fe5ab
Yusuf Manshur <ymanshur@gmail.com>
2023-04-13 15:59:42 +0700

    add worker package using asyncq

---
+++ pkg/worker/distributor.go
@@ -0,0 +1,19 @@
+package worker
+
+import (
+	"github.com/hibiken/asynq"
+)
+
+type TaskDistributor interface {
+}
+
+type RedisTaskDistributor struct {
+	client *asynq.Client
+}
+
+func NewRedisTaskDistributor(redisOpt asynq.RedisClientOpt) TaskDistributor {
+	client := asynq.NewClient(redisOpt)
+	return &RedisTaskDistributor{
+		client: client,
+	}
+}
