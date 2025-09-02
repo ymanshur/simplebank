ecac467788f09a37a4450d311b6e5246ee368f49
Yusuf Manshur <ymanshur@gmail.com>
2025-09-02 22:25:31 +0700

    feat(deployment): tidy up and add gRPC port info

--- deployment/start.sh
+++ deployment/start.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-# the script will exit immediately
+# The script will exit immediately
 # if a command returns a non-zero status
 set -e
 
-# the migration process was moved into running code
-#echo "run db migration"
-#source /app/app.env
-#/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
+# echo "Run db migration"
+# The migration process was moved into running code
+# migrate -path /app/db/migration -database "$DB_SOURCE" -verbose up
 
-echo "start the app"
-# takes all parameters passed to the script and run it
+echo "Start the app"
+# exec takes all parameters passed to the script and run it
 exec "$@"
