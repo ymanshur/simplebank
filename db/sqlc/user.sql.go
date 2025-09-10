d5c07b7891f86777cd218a9ffaeaed42409458cb
Yusuf Manshur <ymanshur@gmail.com>
2025-09-10 13:39:10 +0700

    feat(db/sqlc): generate role field

--- db/sqlc/user.sql.go
+++ db/sqlc/user.sql.go
@@ -19,7 +19,7 @@ INSERT INTO users (
     hashed_password
 ) VALUES (
     $1, $2, $3, $4
-) RETURNING username, full_name, email, hashed_password, password_changed_at, created_at, is_email_verified
+) RETURNING username, full_name, email, hashed_password, password_changed_at, created_at, is_email_verified, role
 `
 
 type CreateUserParams struct {
@@ -45,12 +45,13 @@ func (q *Queries) CreateUser(ctx context.Context, arg CreateUserParams) (User, e
 		&i.PasswordChangedAt,
 		&i.CreatedAt,
 		&i.IsEmailVerified,
+		&i.Role,
 	)
 	return i, err
 }
 
 const getUser = `-- name: GetUser :one
-SELECT username, full_name, email, hashed_password, password_changed_at, created_at, is_email_verified FROM users
+SELECT username, full_name, email, hashed_password, password_changed_at, created_at, is_email_verified, role FROM users
 WHERE username = $1 LIMIT 1
 `
 
@@ -65,6 +66,7 @@ func (q *Queries) GetUser(ctx context.Context, username string) (User, error) {
 		&i.PasswordChangedAt,
 		&i.CreatedAt,
 		&i.IsEmailVerified,
+		&i.Role,
 	)
 	return i, err
 }
@@ -79,7 +81,7 @@ SET
     is_email_verified = COALESCE($5, is_email_verified)
 WHERE
     username = $6
-RETURNING username, full_name, email, hashed_password, password_changed_at, created_at, is_email_verified
+RETURNING username, full_name, email, hashed_password, password_changed_at, created_at, is_email_verified, role
 `
 
 type UpdateUserParams struct {
@@ -109,6 +111,7 @@ func (q *Queries) UpdateUser(ctx context.Context, arg UpdateUserParams) (User, e
 		&i.PasswordChangedAt,
 		&i.CreatedAt,
 		&i.IsEmailVerified,
+		&i.Role,
 	)
 	return i, err
 }
