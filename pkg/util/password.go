ae62ffafcd714d0073b04af1547473c541cd7a3d
Yusuf Manshur <ymanshur@gmail.com>
2025-10-08 08:06:04 +0700

    chore(util/password): unwrap error

--- pkg/util/password.go
+++ pkg/util/password.go
@@ -1,7 +1,6 @@
 package util
 
 import (
-	"fmt"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -9,7 +8,7 @@ import (
 func HashPassword(password string) (string, error) {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
-		return "", fmt.Errorf("failed to hash password: %w", err)
+		return "", err
 	}
 	return string(hashedPassword), nil
 }
